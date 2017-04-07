1938414e75ce08a3cf499efd7dbc037920376cf7
pmarquez20@gmail.com <pmarquez20@gmail.com>
2017-04-07 07:00:38 -0500

    subir

---
+++ Test/src/com/pmp/JFileChooserSelectionMode.java
@@ -0,0 +1,16 @@
+package com.pmp;
+
+import javax.swing.JFileChooser;
+
+public class JFileChooserSelectionMode {
+
+  public static void main(String[] a) {
+
+    JFileChooser fileChooser = new JFileChooser();
+
+    fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+    fileChooser.showOpenDialog(null);
+  }
+
+}
---
+++ Test/src/com/pmp/Main.java
@@ -0,0 +1,32 @@
+package com.pmp;
+/*
+import javax.swing.JFileChooser;
+
+public class Main {
+  public static void main(String[] argv) {
+	  System.out.println("hola");
+    JFileChooser fileChooser = new JFileChooser(".");
+    fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+  }
+}
+*/
+
+import javax.swing.JFileChooser;
+
+public class Main {
+  public static void main(String[] argv) {
+    JFileChooser chooser = new JFileChooser();
+    int result = chooser.showOpenDialog(null);
+    switch (result) {
+    case JFileChooser.APPROVE_OPTION:
+      System.out.println("Approve (Open or Save) was clicked");
+      break;
+    case JFileChooser.CANCEL_OPTION:
+      System.out.println("Cancel or the close-dialog icon was clicked");
+      break;
+    case JFileChooser.ERROR_OPTION:
+      System.out.println("Error");
+      break;
+    }
+  }
+}
